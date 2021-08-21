034297d8c15ee96ce81043691308029bf9692597
Fa Ainama Caldera <faainamacaldera16@gmail.com>
2021-08-21 10:45:25 +0700

    Penambahan Element Pada Home dan About

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        colors : { 
+            brown : { 
+                // TYPOGRAPHY COLOR OR EITHER
+                'light-brown' : "#FFFBF9",
+                'brown' : "#816150",
+                'dark-brown' : "#4B3C3B",
+                
+                // ONLY FOR BACKGROUND-COLOR
+                'bg-brown-semi-dark' : "rgba(228, 216, 212, 0.15)",
+                'bg-brown-dark'  : "#4B3C3B",
+                'bg-brown-light' : "#FFFBF9"
+            }
+      },
+
+        fontFamily : { 
+            // TYPOGRAPHY FROM GOOGLE FONTS
+            'body-font' : ['Lato', 'sans-serif'],
+            'heading-font' : ['Inter' , 'sans-serif']
+        } 
+    }
   },
   variants: {
     extend: {},
